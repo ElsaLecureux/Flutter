c258bb868ecbdb9dcc8f52aaddc84897aef03649
ElsaLecureux <elsa.lecureux@gmail.com>
2024-05-30 11:17:20 +0200

    exo

---
+++ exosflutter/Exos/Exo3.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: Scaffold(
+    appBar: AppBar(title: Text('Ecran de base')),
+    body: Center(
+      child: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 200,
+              height: 150,
+              decoration: BoxDecoration(
+                color:  Color.fromARGB(255, 193, 250, 196),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(10.0), // Uniform radius
+              ),
+              child: Center(
+                child: Text('Gauche'),
+              ),
+              ),
+            Container(
+               width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 248, 250, 138),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(10.0), // Uniform radius
+              ),
+              child: Center(
+                child: Text('Centre'),
+              ),
+            ),
+            Container(
+               width: 400,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 165, 231, 243),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(10.0), // Uniform radius
+              ),
+              child: Center(
+                child: Text('Droite'),
+              ),
+            ),
+        ]   
+    )
+    )
+    
+    )
+    ));
+}
