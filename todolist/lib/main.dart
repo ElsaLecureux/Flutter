2ada8faf22cdd073cd231f7bb28cbb198bd40950
ElsaLecureux <elsa.lecureux@gmail.com>
2024-05-31 10:30:24 +0200

    dreated simple liste and add a button

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  List<String> list = ['Courses', 'Menus de la semaine', ' Yoga', 'Minecraft'];
+
+  void _showInputDialog () {
+    print('done');
+  }
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Ma Liste')
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Vaisselle'),
+          ),
+          ListTile(
+            title: Text('Musique'),
+          ),
+          ListTile(
+            title: Text('Ménage'),
+          ),
+          ListTile(
+            title: Text('Code'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Add an "Edit" icon button that launches the input dialog box when pressed
+      onPressed: _showInputDialog,
+      child: Icon(Icons.add),
+      ),
+    ),
+  ));
+
 }
 
